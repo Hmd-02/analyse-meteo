c5aebf469fe88f47ad79160f3c40861fc88ddf5c
Faïçal Cheick Hamed <168723453+Hmd-02@users.noreply.github.com>
2025-02-20 06:32:27 +0000

    Update df.py

--- df.py
+++ df.py
@@ -7,7 +7,7 @@ import setuptools
 from pyspark.sql.window import Window
 import pyspark.sql.functions as F
 import os
-#os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-17-openjdk-amd64"
+os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-17-openjdk-amd64"
 
 # Initialisation de la session Spark
 spark_session = SparkSession.builder.appName("Analyse").getOrCreate()
